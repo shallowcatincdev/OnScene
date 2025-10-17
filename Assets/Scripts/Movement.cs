7942c663cbce7da8abac8f052d62aaa5fd2f3d22
Shallowcat <joshR364@hotmail.com>
2025-10-17 13:56:20 -0400

    I have no clue what this is. i forgot to commit like 2 months ago.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMove(InputValue value)
+    {
+
+    }
+
+    void OnMouse(InputValue value)
+    {
+
+    }
+
+}
